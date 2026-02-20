77901b68a099d9ef306210c65f769ccb34698b8e
mahek <mahek@MAHEK>
2026-02-20 17:35:04 +0530

    Add project files.

---
+++ Pinkleaf_Collection/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pinkleaf_Collection.Models;
+
+namespace Pinkleaf_Collection.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login(string email = "", string password = "")
+        {
+            ViewBag.Email = email;
+            ViewBag.Password = password;
+            return View();
+        }
+
+        // SIGN UP PAGE
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // SIGN UP POST
+        [HttpPost]
+        
+        public IActionResult Register(RegisterViewModel model)
+        {
+            // abhi DB nahi, sirf redirect
+            return RedirectToAction("Login", new
+            {
+                email = model.Email,
+                password = model.Password
+            });
+
+            return View(model);
+        }
+
+
+      
+        }
+    
+}
---
+++ Pinkleaf_Collection/Models/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pinkleaf_Collection.Models
+{
+    
+    public class RegisterViewModel
+    {
+        [Required]
+        public string FullName { get; set; }
+
+        public string Address { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+    }
