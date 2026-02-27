3cb109754e44a4dd816067532120396705d913a2
Prathna1312 <prathnaravindrakumar1312@gmail.com>
2026-02-27 22:16:32 +0530

    Add files via upload

---
+++ Pinkleaf_Collection/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pinkleaf_collection_.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
+        public IActionResult Categories()
+        {
+            return View();
+        }
+        public IActionResult Products()
+        {
+            return View();
+        }
+        public IActionResult ProductList()
+        {
+            return View();
+        }
+        public IActionResult Wholesale()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+        public IActionResult EditProfile()
+        {
+            return View();
+        }
+    }
+}
