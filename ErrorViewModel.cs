4d40c6d59e4954469a1ff1c3aecbed24dd6fb7ff
Prathna1312 <prathnaravindrakumar1312@gmail.com>
2026-03-24 15:26:31 +0530

    Delete ErrorViewModel.cs

--- ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Pinkleaf_Collection.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
